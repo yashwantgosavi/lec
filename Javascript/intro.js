c2d2bc6eab4719709529107de1c32dbf0e24cee4
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-09-27 11:47:32 +0530

    first commit

---
+++ Javascript/intro.js
@@ -0,0 +1,49 @@
+1) What can we achieve using Javascript?
+   This problem may have one or more correct answers
+    - WEBSITES 
+    - PRESENTATIONS 
+    - GAMES 
+    - MOBILE APPS 
+
+2) What was the initial name for Javascript?
+   This problem has only one correct answer
+    - Script
+    - Livescript
+    -  Mocha 
+    - None of the above
+
+3) When was ECMAscript officially released?
+  This problem has only one correct answer
+    - 1997 
+    - 1995
+    - 1999
+    - 2001
+
+4) Which of the following variable name is not valid in JavaScript?
+   This problem has only one correct answer
+    - var myname = "John";
+    - var my name = "John"; 
+    - var myName = "John";
+    - var my_name = "John";
+
+5) Which of the following are basic data types in Javascript?
+  This problem may have one or more correct answers
+    - integer
+    - number
+    - null
+    - float
+    - string
+    - character
+    - undefined
+    - object
+
+6) How can you declare an integer?
+  This problem has only one correct answer
+    - int a = 10;
+    - number a = 10;
+    - var a = 10;
+    - integer a = 10;
+
+
+
+
