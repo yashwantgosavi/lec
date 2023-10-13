747e0144cdad603dfc4948846977988e15c52742
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-10-13 20:56:40 +0530

    Day 2: Variables, Datatypes

---
+++ JavaScript/Learning/2Day_variablesAndOperators/dataTypes.js
@@ -0,0 +1,44 @@
+/*
+Data Types
+    1. Object Datatypes
+    2. Primitive Datatypes
+*/
+
+/* ****************************************
+ ***           Primitive Datatypes       ***
+ *******************************************/
+
+// // Number (64 bit)
+//         let num;
+//         num = 23;
+//         console.log(typeof num + " = "+ num);
+
+// // String
+//         const firstName = "yashwant";
+//         const lastName = 'yashwant'; // "yashwant's"
+//         // const nickname =  `No name`; // Backtick Mark
+//         console.log(lastName);
+//         console.log(typeof lastName);
+
+// // Boolean
+// let married = true; // true, false
+// console.log(married);
+
+
+// // Undefined
+//       let a;
+//       console.log(a);
+
+// // Null
+//      let b = null;
+//      console.log(b);
+//      console.log(typeof b);  // object --> Bug
+
+// // Symbol (ES6/2015)
+
+// // BigInt (ES2020)
+
+// //  Intersting things
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE * 3);
