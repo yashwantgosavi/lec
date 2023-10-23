83d563991d9eaf13f3469a41b9b54042b96da9f1
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-10-23 20:38:56 +0530

    js Conditions

--- JavaScript/Learning/4Day_conditionalStatements/problems/gradingSystem.js
+++ JavaScript/Learning/4Day_conditionalStatements/problems/gradingSystem.js
@@ -3,7 +3,7 @@ if the value of marks variable is greater than or equal to 90, the value of resu
 marks  90>
        A grade
 
-marks in between  80 & 89
+marks in between  80  &  89
        B grade
 
 marks in between 70 & 79
@@ -19,3 +19,21 @@ marks is less than 35
        failed
 */
 
+
+
+let marks = prompt("Enter your total score");
+
+if (marks > 90) {
+       console.log("A Grade")
+} else if (marks >= 80 && marks <= 89) {
+       console.log("B Grade")
+}
+else if (marks < 60) {
+       console.log("F Grade")
+}
+else {
+       console.log("num not found")
+}
+
+
+let result;
