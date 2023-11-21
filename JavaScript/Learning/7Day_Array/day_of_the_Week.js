3e557234b5d42ed5a2a4d2338c42fab09eb966e3
yashwantgosavi <yashwantgosavi0@gmail.com>
2023-11-21 19:35:22 +0530

    array

---
+++ JavaScript/Learning/7Day_Array/day_of_the_Week.js
@@ -0,0 +1,7 @@
+/*
+Write a function 'returnDay' that takes one parameter(number from 1 to 7) and returns the day of the week.
+(where 1 is Monday, 2 is Tuesday, 3 is Wednesday, etc).
+
+If the number is less than 1 or greater than 7, the function should return null.
+Note: Store the days of the week in the array.
+*/ 
---
+++ JavaScript/Learning/7Day_Array/duplicate.js
@@ -0,0 +1,5 @@
+/*
+Implement a function “duplicate” to duplicate an array, as shown below:
+For input arr=[1,2] duplicate(arr) should return [1,2,1,2]
+
+*/
