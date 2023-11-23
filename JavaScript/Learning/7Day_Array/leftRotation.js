6707440b13d9d2d80e028779b0c662e11b6a7b51
Yashwant Gosavi <113498322+yashwantgosavi@users.noreply.github.com>
2023-11-23 20:17:52 +0530

    Update leftRotation.js

--- JavaScript/Learning/7Day_Array/leftRotation.js
+++ JavaScript/Learning/7Day_Array/leftRotation.js
@@ -4,3 +4,19 @@ Implement a function that returns an updated array with 1 left rotation on an ar
 rotateLeft([1,2,3,4]) // returns [2,3,4,1]
 
 */ 
+
+
+function rotateLeft(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    // Perform left rotation
+    const firstElement = arr.shift();
+    arr.push(firstElement); 
+    return arr;
+}
+
+// Example usage:
+let originalArray = [1, 2, 3, 4];
+let rotatedArray = rotateLeft(originalArray);
+console.log(rotatedArray);
