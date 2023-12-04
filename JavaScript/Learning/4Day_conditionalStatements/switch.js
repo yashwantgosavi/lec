4dd05098a1558417e9d1db6ad3431ec66578b38c
yashwant gosavi <yashwantgosavi0@gmail.com>
2023-12-04 15:03:40 +0530

    js update

--- JavaScript/Learning/4Day_conditionalStatements/switch.js
+++ JavaScript/Learning/4Day_conditionalStatements/switch.js
@@ -1,9 +1,13 @@
 // switch
-switch (expression) {
-    case value:
-        
-        break;
 
+let day = prompt("Enter Day");
+switch (day) {
+    case 1:
+        console.log("Monday"); 
+        break;
+        case 2:
+            console.log("Tue"); 
+            break;
     default:
         break;
 }
